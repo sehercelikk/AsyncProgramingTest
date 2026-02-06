83b515fab6620e2e8aa3a86c1dfdb7a5aac4e989
Seher ÇELİK <yldzzseher6@gmail.com>
2026-02-06 17:34:11 +0300

    Thread Sleep

--- ders/Program.cs
+++ ders/Program.cs
@@ -14,15 +14,19 @@ thread.Start();
 #endregion
 
 #region Locking Mekanizması
+// Birden fazla thread ın aynı kaynağa aynı anda ulaşma isteğini kontrol etmek için
+// Aynı anda sadece tek bir threadın belirli bir kod bloğuna erişimini sağlamakta ve böylece paylaşılan kaynakların birden fazla thread in müdahalesini engelleyerek Race conditions durumlarına engel olmakta.
+
 object _locking = new();
 int i = 1;
 Thread lock1 = new(() =>
 {
     lock (_locking)
     {
-        while(i <= 10)
+        while(i < 10)
         {
-            Console.WriteLine(i++);
+            i++;
+            Console.WriteLine($"Thread 1: {i}");
            
         }
     }
@@ -32,9 +36,10 @@ Thread lock2 = new(() =>
 {
     lock(_locking)
     {
-        while (i >1)
+        while (i >0)
         {
-            Console.WriteLine(i--);
+            i--;
+            Console.WriteLine($"Thread 2: {i}");
             
         }
     }
@@ -43,4 +48,18 @@ Thread lock2 = new(() =>
 lock1.Start();
 lock2.Start();
 
+#endregion
+
+#region Thread.Sleep 
+// Belirli saniye uyutmak için
+Thread threadSleep = new(() =>
+{
+    for(int i = 0; i < 10; i++)
+    {
+        Console.WriteLine(i);
+        Thread.Sleep(1000);
+    }
+});
+
+threadSleep.Start();
 #endregion
